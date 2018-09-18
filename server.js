d197ce864e78643612d24a43db3f80bd7fd2b877
richjimdev <richjimdev@gmail.com>
2018-09-18 10:45:41 -0700

    scaffold index.ejs

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 'use strict';
 
 const express = require('express');
+const pg = require('pg');
+const ejs = require('ejs');
 require('dotenv').config();
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
 const app = express();
 
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+
+client.connect();
+client.on('error', err => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('BOOK IT! (comming soon)');
+  client.query('SELECT * FROM books;')
+    .then( (result) => {
+      res.render('index', {
+        data: result.rows
+      });
+    });
 });
 
 app.listen(PORT, () => {
